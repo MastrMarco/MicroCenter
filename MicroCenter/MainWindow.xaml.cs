dbf538fda373fdcf1b32a7d68cd86fe3195c989a
MastrMarco <76437833+MastrMarco@users.noreply.github.com>
2024-06-17 16:42:05 +0200

    Ep2

--- MicroCenter/MainWindow.xaml.cs
+++ MicroCenter/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using MicroCenter.Pagine;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,45 @@ namespace MicroCenter
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //Sposta Finestra
+        private void BarraSuperiore_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        //Chiudi Finestra
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        //Nascondi Finestra
+        private void btnNascondi_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized; // Minimizza la finestra
+        }
+
+
+        //Visualiza le Impostazioni
+        private void btnImpostazioni_Click(object sender, RoutedEventArgs e)
+        {
+           // frameContent.Padding = new Thickness(10);
+            frameContent.Navigate(new Impostazioni());
+        }
+        //Visualiza la Connessione
+        private void btnConnessione_Click(object sender, RoutedEventArgs e)
+        {
+            frameContent.Navigate(new Connessione());
+        }
+        //Visualiza Set Arduino 
+        private void btnArduino_Click(object sender, RoutedEventArgs e)
+        {
+            frameContent.Navigate(new Arduino());
         }
     }
 
---
+++ MicroCenter/Pagine/Impostazioni.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MicroCenter.Temi;
+
+namespace MicroCenter.Pagine
+{
+    /// <summary>
+    /// Logica di interazione per Impostazioni.xaml
+    /// </summary>
+    public partial class Impostazioni : Page
+    {
+
+        public Impostazioni()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+        //Swich Imposta il Tema
+        private void Themes_Click(object sender, RoutedEventArgs e)
+        {
+            if (Themes.IsChecked == true)
+                ControllerTemi.SetTheme(ControllerTemi.ThemeTypes.Dark);
+            else
+                ControllerTemi.SetTheme(ControllerTemi.ThemeTypes.Light);
+        }
+
+
+
+
+    }
+}
